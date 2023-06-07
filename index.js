d91998e4d9fd4407c9b0cfd709b39b13a539418c
Joanadgomes <joanadiogog@gmail.com>
2023-06-07 14:02:02 +0100

    readme

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Pokemon: apanhá-los todos
-// O Ash está a apanhar pokémons num mundo que consiste numa grelha bidimensional infinita de casas. Em cada casa há exatamente um pokémon.
-// O Ash começa por apanhar o pokémon que está na casa onde começa. A seguir, move-se para a casa imediatamente a norte, sul, este ou oeste de onde se encontra e apanha o pokémon que aí se encontrar, e assim sucessivamente. Atenção: se ele passar numa casa onde já passou (e, portanto, onde já apanhou um pokémon), já lá não está um pokémon para ele apanhar!
-// O que queremos saber é: começando com um mundo cheio de pokémons (um em cada casa!), quantos pokémons o Ash apanha para uma dada sequência de movimentos?
 
 function ashPokemons(input) {
   let pokemons = 1; // the pokemon at the initial position
